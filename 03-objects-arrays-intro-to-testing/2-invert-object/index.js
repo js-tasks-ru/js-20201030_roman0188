4fb34f052066c5d92caf09c4e0f863c1842424d6
Roman Efimov <roman_efimov_@mail.ru>
2020-11-11 10:01:50 +0300

    add 2-invert-object

--- 03-objects-arrays-intro-to-testing/2-invert-object/index.js
+++ 03-objects-arrays-intro-to-testing/2-invert-object/index.js
@@ -4,5 +4,21 @@
  * @returns {object | undefined} - returns new object or undefined if nothing did't pass
  */
 export function invertObj(obj) {
+  if (!obj) return;
 
+  if (obj) {
+    let keyArr = [];
+    for(let key in obj){
+      keyArr.push(key);
+    }
+    let ValueArr = [];
+    for(let key in obj) {
+      ValueArr.push(obj[key]);
+    }
+    let newUser = {};
+    for (let i = 0; i < keyArr.length; i++) {
+      newUser[ValueArr[i]] = keyArr[i];
+    }
+    return newUser;
+  }
 }
