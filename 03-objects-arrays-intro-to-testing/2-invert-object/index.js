df0de2137a4e4bc407faed279d727233a2aafb58
Roman Efimov <roman_efimov_@mail.ru>
2020-11-27 14:18:17 +0300

    add 2-invert-object, uniq, column-chart

--- 03-objects-arrays-intro-to-testing/2-invert-object/index.js
+++ 03-objects-arrays-intro-to-testing/2-invert-object/index.js
@@ -6,19 +6,8 @@
 export function invertObj(obj) {
   if (!obj) return;
 
-  if (obj) {
-    let keyArr = [];
-    for(let key in obj){
-      keyArr.push(key);
-    }
-    let ValueArr = [];
-    for(let key in obj) {
-      ValueArr.push(obj[key]);
-    }
-    let newUser = {};
-    for (let i = 0; i < keyArr.length; i++) {
-      newUser[ValueArr[i]] = keyArr[i];
-    }
-    return newUser;
-  }
+  return Object.entries(obj).reduce((accum, [key, value]) => {
+    accum[value] = key;
+    return accum;
+  }, {});
 }
--- 03-objects-arrays-intro-to-testing/4-uniq/index.js
+++ 03-objects-arrays-intro-to-testing/4-uniq/index.js
@@ -4,12 +4,10 @@
  * @returns {*[]} - the new array with uniq values
  */
 export function uniq(arr) {
-  const result = [];
-  const Obj = new Set(arr);
+  const obj = {};
 
-  for (let item of Obj) {
-    result.push(item);
-
-  }
-  return result;
+  arr.forEach(item => {
+    obj[item] = item;
+  });
+  return Object.values(obj);
 }
